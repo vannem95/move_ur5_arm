ba89daf535f9652ae067fefd5429ef6e559313d3
Vivek Annem <vannem@ncsu.edu>
2018-01-23 16:15:10 -0800

    built gripper node

---
+++ gripper_node.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import serial
+import time
+import rospy
+from std_msgs.msg import Bool
+
+ser = serial.Serial(port='/dev/ttyUSB0',baudrate=115200,timeout=1,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
+
+time.sleep(0.05)
+
+def open_close(switch):
+# opens / closes the gripper
+    if 1 == switch:
+        print "Closing gripper"
+        ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\xFF\xFF\x42\x29")
+        time.sleep(1)
+
+    if 0 == switch:
+        print "Opening gripper"
+        ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19")
+        time.sleep(1)
+    
+
+def gripper_node():
+# subscribes to gripper_state topic
+    rospy.init_node('gripper_node', anonymous=True)
+    rospy.Subscriber("gripper_state", Bool, open_close)
+    rospy.loginfo('Subscribing to gripper_state to open/close the gripper')
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        gripper_node()
+    except rospy.ROSInterruptException:
+        pass
