1df34a20b658b553ea69140b8c780e9fa40d15d3
Vivek Annem <vannem@ncsu.edu>
2018-03-07 00:30:39 -0800

    edited grasp mode

--- ur5_line_move_server.py
+++ ur5_line_move_server.py
@@ -62,9 +62,11 @@ def move_to_waypoint(req):
     elif "close" == req.a:
         ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\xFF\xFF\x42\x29")
         time.sleep(1)
+        point = last_point
     elif "open" == req.a:
         ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19")
         time.sleep(1)
+        point = last_point
     else:
         print "wrong mode option"
 
