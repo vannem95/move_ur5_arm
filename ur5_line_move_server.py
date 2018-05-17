0f7841aecd62594c5b4d46bd1260e31058dc718f
Vivek Annem <vannem@ncsu.edu>
2018-05-16 20:38:44 -0700

    uncommented gripper and removed old home, midpt and dest

--- ur5_line_move_server.py
+++ ur5_line_move_server.py
@@ -10,7 +10,7 @@ import math as m
 from std_msgs.msg import Bool
 
 
-last_point = [110.38/1000,-323.24/1000,-620.19/1000,3.1299,-0.0030,-0.0158]
+last_point = [-332.32/1000,-106.95/1000,-52.41/1000,0.0607,-2.7290,-0.0871]
 sleep_time = 0
 
 HOST = '192.168.1.6'     # The remote host
@@ -23,38 +23,38 @@ s.connect((HOST, PORT))
 time.sleep(0.05)
 
 
-# ser = serial.Serial(port="/dev/ttyUSB0",baudrate=115200,timeout=1,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
+ser = serial.Serial(port="/dev/ttyUSB0",baudrate=115200,timeout=1,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
 
-# time.sleep(0.05)
+time.sleep(0.05)
 
-# ############# Auto Gripper test --- START --- #####################
+############# Auto Gripper test --- START --- #####################
 
-# ser.write("\x09\x10\x03\xE8\x00\x03\x06\x00\x00\x00\x00\x00\x00\x73\x30")
+ser.write("\x09\x10\x03\xE8\x00\x03\x06\x00\x00\x00\x00\x00\x00\x73\x30")
 
-# data_raw = ser.readline()
-# time.sleep(0.01)
+data_raw = ser.readline()
+time.sleep(0.01)
 
-# ser.write("\x09\x03\x07\xD0\x00\x01\x85\xCF")
+ser.write("\x09\x03\x07\xD0\x00\x01\x85\xCF")
 
-# data_raw = ser.readline()
-# time.sleep(1)
+data_raw = ser.readline()
+time.sleep(1)
 
-# print "Activating Gripper"
-# print "Now closing gripper.."
+print "Activating Gripper"
+print "Now closing gripper.."
 
-# ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\xFF\xFF\x42\x29")
+ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\xFF\xFF\x42\x29")
 
-# data_raw = ser.readline()
-# time.sleep(2)
+data_raw = ser.readline()
+time.sleep(2)
 
-# print "Now opening gripper.."
+print "Now opening gripper.."
 
-# ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19")
+ser.write("\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19")
 
-# data_raw = ser.readline()
-# time.sleep(2)
+data_raw = ser.readline()
+time.sleep(2)
 
-# print "Gripper ready"
+print "Gripper ready"
 
 ############# Auto Gripper test --- END --- #####################
 
@@ -69,16 +69,8 @@ def move_to_waypoint(req):
     global sleep_time
 
     if "home" == req.a:
-        point = [110.38/1000,-363.24/1000,-610.19/1000,3.1299,-0.0030,-0.0158]
-
-    elif "midpoint" == req.a:
-        point = [110.39/1000,-363.23/1000,97.79/1000,3.1299,-0.0030,-0.0159]
-
-    elif "home2" == req.a:
         point = [-332.32/1000,-106.95/1000,-52.41/1000,0.0607,-2.7290,-0.0871]
 
-    elif "destination" == req.a:
-        point = [-324.98/1000,-127.38/1000,130.18/1000,2.2745,-2.1644,-0.0242]
 
     elif "grasp" == req.a:
         point = [-564.06/1000,-89.90/1000,123.39/1000,0.1956,4.7207,0.0671]
@@ -120,7 +112,7 @@ def move_to_waypoint(req):
 
     last_point = point
 
-    s.send("movel(p[" + str(point[0]) + ", " + str(point[1]) + ", " + str(point[2]) + ", " + str(point[3]) + ", " + str(point[4]) + ", " + str(point[5]) + "], a=0.2, v=0.1)\n")
+    s.send("movel(p[" + str(point[0]) + ", " + str(point[1]) + ", " + str(point[2]) + ", " + str(point[3]) + ", " + str(point[4]) + ", " + str(point[5]) + "], a=0.2, v=0.2)\n")
     print(" the command send to socket is: movel(p[" + str(point[0]) + ", " + str(point[1]) + ", " + str(point[2]) + ", " + str(point[3]) + ", " + str(point[4]) + ", " + str(point[5]) + "], a=0.2, v=0.2)\n")
     time.sleep(2)
     return ur5_line_moveResponse("reached")
@@ -130,7 +122,7 @@ def ur5_line_move_server():
 # starts a service that takes direction(up/down/left/right/forward/backward)/checkpoint() and distance/0 
     rospy.init_node('ur5_line_move_server', anonymous=True)
     serv = rospy.Service('ur5_line_move_service', ur5_line_move, move_to_waypoint)
-    rospy.loginfo('Service initiated...\n\nArguments are \n\n1. Direction/mode {{home/midpoint/home2/destination/grasp/open/close} {up/down/left/right/forward/backward -- from last point} }\n\n2. Distance(in mm) {""0"" if a checkpoint is chosen}\n\n\n\n CHECK THE POSE OF THE ROBOT THEN CHOOSE THE MODE AND DISTANCE ACCORDINGLY')
+    rospy.loginfo('Service initiated...\n\nArguments are \n\n1. Direction/mode {{ home / grasp / open / close} { up / down / left / right / forward / backward -- from last point} }\n\n2. Distance(in mm) {""0"" if a checkpoint is chosen}\n\n\n\n CHECK THE POSE OF THE ROBOT THEN CHOOSE THE MODE AND DISTANCE ACCORDINGLY')
     rospy.spin()
 
 if __name__ == '__main__':
